e2fdfdd03acdad6d802bfc50248368e1896e88cd
ConacBreslin <conacbreslin@sky.com>
2021-06-12 15:20:21 +0000

    functioninng changing background

--- quiz.js
+++ quiz.js
@@ -5,20 +5,36 @@ let optionC = document.getElementById("C");
 let optionD = document.getElementById("D");
 let counter = document.getElementById("timeRemainingArea");
 let options = document.getElementsByClassName("option");
+let playerPickNumber = document.getElementById("playerPickNumber");
+let pickMax = document.getElementById("pickMax");
+let form = document.getElementById("pickForm");
 
-var correctAnswer; 
+var correctAnswer;
 let currentQuestionNumber = 0;
 let score = 0;
 let availableQuesions = [];
-let maxQuestions = 10;
+let maxQuestions;
 let TIMER;
 let questionTime = 10;
 let gaugeWidth = 150;
 let gaugeUnit = gaugeWidth / questionTime;
+let classToApply = "incorrect"
 
+// set maximum number of questions
+pickMax.addEventListener("click", setMaxNumber);
 
+function setMaxNumber(event) {
+    event.preventDefault();
+    maxQuestions = playerPickNumber.value;
+    console.log(maxQuestions);
+    if (maxQuestions <= 20) {
+        pickForm.classList.add("d-none");
+        startQuiz();
+    }else{
+        alert("You must pick a number between 1 and 20");
+    }
 
-
+}
 // funtion to display question and options and increase current question number
 function renderQuestion() {
     if (availableQuestions.length === 0 || currentQuestionNumber >= maxQuestions) {
@@ -52,14 +68,25 @@ function addEventListenersToOptions() {
     }
 }
 
-function checkAnswer(event){
-    let target = event.currentTarget.id;
-    console.log(target);
-    console.log(correctAnswer);
-    if (target === correctAnswer){};
+function checkAnswer(event) {
+    let targetValue = event.currentTarget.id;
+    let targetElement = event.currentTarget;
+    console.log({correctAnswer});
+    console.log(targetValue);
+   
+    if (targetValue === correctAnswer) {
+        score++;
+        classToApply = "correct";
+        targetElement.classList.add(classToApply);
+        setTimeout(function(event){ targetElement.classList.remove(classToApply); renderQuestion();}, 1000)
+    } else {
+        classToApply = "incorrect";
+        targetElement.classList.add(classToApply);
+        setTimeout(function(event){ targetElement.classList.remove(classToApply); renderQuestion(); }, 1000)
+            
+    } 
+    }
 
-
-}
 
 
 function renderCounter() {
--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-console.log("hello form the script");
 let displayQuestionNumbers = document.getElementById("displayQuestionNumbers");
-console.log(displayQuestionNumbers);
 displayQuestionNumbers.addEventListener("click", displayQuestionsWanted);
 let questionsWanted = document.getElementsByClassName("questionsWanted");
-
 function displayQuestionsWanted() {
     for (let i of questionsWanted) {
-        console.log("displayQuestionsWanted clicked")
+        
         i.classList.remove("d-none");
 
 
