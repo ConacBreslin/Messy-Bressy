3bbd65fbeb9b22b8d62d3f47acdab9b19276398d
ConacBreslin <conacbreslin@sky.com>
2021-06-13 14:56:15 +0000

    counter added

--- quiz.js
+++ quiz.js
@@ -8,22 +8,23 @@ let options = document.getElementsByClassName("option");
 let playerPickNumber = document.getElementById("playerPickNumber");
 let pickMax = document.getElementById("pickMax");
 let form = document.getElementById("pickForm");
-let quizToAppear = document.getElementById("quizToAppear");
 let hudQuestion = document.getElementById("hudQuestion");
 let hudScore = document.getElementById("hudScore");
-let progressBarFull = document.getElementById("progressBarFull")
-
+let progressBarFull = document.getElementById("progressBarFull");
+let timeRemaining = document.getElementById("timeRemainingArea")
 
 var correctAnswer;
 let currentQuestionNumber = 0;
 let score = 0;
 let availableQuesions = [];
 let maxQuestions;
-let TIMER;
+let classToApply;
+let count = 0;
 let questionTime = 10;
 let gaugeWidth = 150;
 let gaugeUnit = gaugeWidth / questionTime;
-let classToApply;
+let TIMER;
+
 
 // set maximum number of questions
 pickMax.addEventListener("click", setMaxNumber);
@@ -31,8 +32,7 @@ pickMax.addEventListener("click", setMaxNumber);
 function setMaxNumber(event) {
     event.preventDefault();
     maxQuestions = playerPickNumber.value;
-    console.log(maxQuestions);
-    if (maxQuestions <= 20) {
+        if (maxQuestions <= 20) {
         pickForm.classList.add("d-none");
         startQuiz();
     }else{
@@ -40,30 +40,36 @@ function setMaxNumber(event) {
     }
 
 }
-// funtion to display question and options and increase current question number
+// funtion to display question and options and increase current question number and set HUD progress
 function renderQuestion() {
-    if (availableQuestions.length === 0 || currentQuestionNumber >= maxQuestions) {
-        //go to end page
-        return window.location.assign('/end.html')
+    if (currentQuestionNumber >= maxQuestions) {
+        console.log(score);
+        localStorage.setItem("finalScore", score);
+        localStorage.setItem("maxQuestions", maxQuestions);
+        //go to finalscore page
+        return window.location.assign('/finalscore.html')
     }
     currentQuestionNumber++;
     hudQuestion.innerText = `Question: ${currentQuestionNumber}/${maxQuestions}`;
     //UpdateProgressBar
     progressBarFull.style.width = `${(currentQuestionNumber/maxQuestions) * 100}%`;
-    
     let questionIndex = Math.floor(Math.random() * availableQuestions.length); // create a random number based on number of questions available
+
     currentQuestion = availableQuestions[questionIndex];
+
     phobia.innerHTML = `<p>${currentQuestion.phobia} is the fear of ...</p>`; // set phobia to be guessed
     optionA.innerHTML = `<p>${currentQuestion.choiceA.icon}</p><p>${currentQuestion.choiceA.name}</p>`
+
     optionB.innerHTML = `<p>${currentQuestion.choiceB.icon}</p><p>${currentQuestion.choiceB.name}</p>`
+
     optionC.innerHTML = `<p>${currentQuestion.choiceC.icon}</p><p>${currentQuestion.choiceC.name}</p>`
+
     optionD.innerHTML = `<p>${currentQuestion.choiceD.icon}</p><p>${currentQuestion.choiceD.name}</p>`
     // sets four different options 
     correctAnswer = currentQuestion.correct
     availableQuestions.splice(questionIndex, 1); //remove question used from available question array
+
     addEventListenersToOptions();
-    
-    
 }
 
 
@@ -72,15 +78,15 @@ function addEventListenersToOptions() {
         i.addEventListener('click', checkAnswer);
     }
 }
-
+//code to checkAnswer, record score, change  background and  set HUD score
 function checkAnswer(event) {
     let targetValue = event.currentTarget.id;
     let targetElement = event.currentTarget;
-    console.log({correctAnswer});
-    console.log(targetValue);
-     if (targetValue === correctAnswer) {
+       
+    if (targetValue === correctAnswer) {
         score++;
         hudScore.innerText = `${score}`;
+        classToApply = "correct";
         classToApply = "correct";
         targetElement.classList.add(classToApply);
         setTimeout(function(event){ targetElement.classList.remove(classToApply); renderQuestion();}, 1000)
@@ -88,20 +94,19 @@ function checkAnswer(event) {
         classToApply = "incorrect";
         targetElement.classList.add(classToApply);
         setTimeout(function(event){ targetElement.classList.remove(classToApply); renderQuestion(); }, 1000)
+            
     } 
     }
 
-
-function renderCounter() {
-    let count = 10;
-    if (count <= questionTime) {
-        counter.innerHTML = count;
-        // timeGauge.style.width = count * gaugeUnit;
-        count--
-    } else {
-        count = 10
+    function renderCounter(){
+        if (count <= questionTime){
+            counter.innerHTML = `<p>${count}</p>`;
+            timeGauge.style.width = count * gaugeUnit + "px";
+            count++
+        }else{
+            count = 0;
+        }
     }
-}
 
 
 
@@ -119,7 +124,7 @@ function startQuiz() {
 
     renderQuestion(); // display question and options and increase current question number
     renderCounter(); // display time remaining for each question
-    TIMER = setInterval(renderCounter, 1000); // call timer every second
+   TIMER = setInterval(renderCounter, 1000); // call timer every second
     
 
 }
